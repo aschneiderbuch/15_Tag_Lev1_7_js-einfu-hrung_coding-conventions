d9a603cc96d15df3b15cc038248a763f6a5e94f2
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-01-03 12:48:03 +0100

    Ende

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+/* 
+helloworld = Hello World, 
+pi = 3.14, 
+burjdubai = 828m, 
+fullname = Jan Schmidt, 
+eifeltower = 324m
+camelcase = camelCase,
+kursstatus = true
+ */
+
+let helloWorld = "Hello World"
+console.log(helloWorld)
+
+const pi = 3.14                     // Number = Zahl = in Frabe lila
+console.log(pi)
+
+const burjDubai = "828m"            // String = Text
+console.log(burjDubai)
+
+let fullName = "Jan Schmidt"
+console.log(fullName)
+
+const eifelTower = "324m"
+console.log (eifelTower)
+
+let camelCase ="camelCase"
+console.log(camelCase)
+
+let kursStaus = true                // Boolean
+console.log(kursStaus)
